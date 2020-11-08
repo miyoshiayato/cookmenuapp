b9ec2301c03c2136b546e39895d33b7a74084ad5
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-11-08 19:21:29 +0900

    full_titleメソッドを定義

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = 'クックメニュー'
+    if page_title.blank?
+      base_title  
+    else
+      "#{page_title} - #{base_title}" # トップ以外のページはタイトル「利用規約 - クックメニュー」（例）
+    end
+  end
 end
