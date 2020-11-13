398e3cab03c086698fd035920e730f540b2d2426
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-11-13 20:04:59 +0900

    admin_userを追記

--- spec/requests/users_index_spec.rb
+++ spec/requests/users_index_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.describe "ユーザー一覧ページ", type: :request do
   let!(:user) { create(:user) }
+  let!(:admin_user) { create(:user, :admin) }
 
   context "認可されたユーザーの場合" do
     it "レスポンスが正常に表示されること" do
