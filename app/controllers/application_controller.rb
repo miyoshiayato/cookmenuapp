027edfc541550ed10f17a7b76d5caeefe5a86e2f
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-11-13 19:19:17 +0900

    アクセスしようとしていたURLを保存する処理を追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ private
     # ログイン済みユーザーかどうか確認
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "ログインしてください"
         redirect_to login_url
       end
