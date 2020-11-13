7aab007c2ce801b203aac6a5f0927b95ed0bf625
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-11-13 19:44:37 +0900

    destroyアクション追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
+
   def index
   end
 
@@ -35,26 +37,31 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    flash[:success] = "アカウントを削除しました"
+    redirect_to root_url
+  end
+
   def index
     @users = User.paginate(page: params[:page])
   end
 
-  private
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
+private
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-    end
+  end
 
-    def user_params_update
-      params.require(:user).permit(:name, :email, :introduction, :sex)
-    end
+  def user_params_update
+    params.require(:user).permit(:name, :email, :introduction, :sex)
+  end
 
-    def correct_user
-      @user = User.find(params[:id])
-      if !current_user?(@user)
-        flash[:danger] = "このページへはアクセスできません"
-        redirect_to(root_url)
-      end
+  def correct_user
+    @user = User.find(params[:id])
+    if !current_user?(@user)
+      flash[:danger] = "このページへはアクセスできません"
+      redirect_to(root_url)
     end
   end
+end
