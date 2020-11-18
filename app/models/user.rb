36e4fc0b610567965ade1cd99936121524f11b08
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-11-18 19:27:25 +0900

    has_many :following追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
                                   dependent: :destroy
+  has_many :following, through: :active_relationships,  source: :followed
   attr_accessor :remember_token
   before_save :downcase_email
   validates :name, presence: true, length: { maximum: 50 }
