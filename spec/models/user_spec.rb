a224eaa528b5f6204c0f5fa9484730d4122a5b6f
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-11-18 19:36:21 +0900

    Userモデルのテストに、追加したメソッドのテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -63,4 +63,15 @@ RSpec.describe User, type: :model do
       expect(user.authenticated?('')).to eq false
     end
   end
+
+  context "フォロー機能" do
+    it "フォローとアンフォローが正常に動作すること" do
+      expect(user.following?(other_user)).to be_falsey
+      user.follow(other_user)
+      expect(user.following?(other_user)).to be_truthy
+      expect(other_user.followed_by?(user)).to be_truthy
+      user.unfollow(other_user)
+      expect(user.following?(other_user)).to be_falsey
+    end
+  end
 end
