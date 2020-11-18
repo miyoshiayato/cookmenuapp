9fa2d1a1bb72a3744df62386652455d54c1a41ec
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-11-18 19:57:50 +0900

    Usersコントローラにfollowing、followersメソッドを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy, :following, :followers]
 
   def index
   end
