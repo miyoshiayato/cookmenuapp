b434ca3e402b925584ab6dc722f90c0bb84f52d0
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-11-19 22:30:35 +0900

    add_indexを追記

--- db/migrate/20201119132158_create_favorites.rb
+++ db/migrate/20201119132158_create_favorites.rb
@@ -3,8 +3,8 @@ class CreateFavorites < ActiveRecord::Migration[5.2]
     create_table :favorites do |t|
       t.integer :user_id
       t.integer :dish_id
-
       t.timestamps
     end
+    add_index :favorites, [:user_id, :dish_id], unique: true
   end
 end
