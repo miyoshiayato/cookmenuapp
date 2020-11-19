0e95cc0bf85acc27290f853f7ebf3102a2cd3825
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-11-19 22:50:13 +0900

    createアクション追記

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,6 +2,12 @@ class FavoritesController < ApplicationController
   before_action :logged_in_user
 
   def create
+    @dish = Dish.find(params[:dish_id])
+    @user = @dish.user
+    current_user.favorite(@dish)
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
   end
 
   def destroy
