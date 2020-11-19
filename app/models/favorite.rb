2d79b422becab545a2d183a9583001dbb52da626
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-11-19 22:34:45 +0900

    コメントアウト削除

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,6 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :dish
-  validates :user_id, presence: true  # 追記
+  validates :user_id, presence: true
   validates :dish_id, presence: true 
 end
