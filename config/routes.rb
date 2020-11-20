8721f9592f6a1e966cde51d2513dff0e149b4448
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-11-20 20:42:32 +0900

    ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   end
   resources :dishes 
   resources :relationships, only: [:create, :destroy]
+  resources :comments, only: [:create, :destroy]
   get    :login,     to: 'sessions#new'
   post   :login,     to: 'sessions#create'
   delete :logout,    to: 'sessions#destroy'
