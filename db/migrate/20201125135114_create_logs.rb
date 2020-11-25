e261e92963c8e94ca07f69777f8bb9571fbae197
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-11-25 22:52:34 +0900

    add_indexを追加

--- db/migrate/20201125135114_create_logs.rb
+++ db/migrate/20201125135114_create_logs.rb
@@ -3,8 +3,8 @@ class CreateLogs < ActiveRecord::Migration[5.2]
     create_table :logs do |t|
       t.integer :dish_id
       t.text :content
-
       t.timestamps
     end
+    add_index :logs, :dish_id
   end
 end
