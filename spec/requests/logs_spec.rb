6be65eb7654a0d02945280e9a10a0f686dd6076f
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-11-25 23:16:37 +0900

    ログ登録時にログインしていない場合、ログ登録できず、ログインページへリダイレクトすることのテスト追加

--- spec/requests/logs_spec.rb
+++ spec/requests/logs_spec.rb
@@ -6,6 +6,16 @@ RSpec.describe "ログ機能", type: :request do
   context "ログ登録" do
     context "ログインしている場合" do
     end
+
+    context "ログインしていない場合" do
+      it "ログ登録できず、ログインページへリダイレクトすること" do
+        expect {
+          post logs_path, params: { dish_id: dish.id,
+                                    log: { content: "良い味付けで作れた" } }
+        }.not_to change(dish.logs, :count)
+        expect(response).to redirect_to login_path
+      end
+    end
   end
 
   context "ログ削除" do
