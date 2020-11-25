fe943a84a761137f3105fd2832c256f016eb192a
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-11-25 23:06:33 +0900

    バリデーションが有効な状態をテストの追加

--- spec/models/log_spec.rb
+++ spec/models/log_spec.rb
@@ -3,4 +3,9 @@ require 'rails_helper'
 RSpec.describe Log, type: :model do
   let!(:log) { create(:log) }
 
+  context "バリデーション" do
+    it "有効な状態であること" do
+      expect(log).to be_valid
+    end
+
 end
