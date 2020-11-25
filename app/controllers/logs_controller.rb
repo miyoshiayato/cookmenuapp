67c1c01c0a9566a72013c6d47e4905aebcc767b1
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-11-25 23:38:15 +0900

    createアクション追記

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -2,6 +2,11 @@ class LogsController < ApplicationController
   before_action :logged_in_user
 
   def create
+    @dish = Dish.find(params[:dish_id])
+    @log = @dish.logs.build(content: params[:log][:content])
+    @log.save
+    flash[:success] = "クックログを追加しました！"
+    redirect_to dish_path(@dish)
   end
 
   def destroy
