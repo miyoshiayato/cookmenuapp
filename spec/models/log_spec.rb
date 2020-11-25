8dc105bd210c765b4d4013d25f76586c2e00b719
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-11-25 23:07:07 +0900

    dish_idがなければ無効な状態であることのテストを追加

--- spec/models/log_spec.rb
+++ spec/models/log_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe Log, type: :model do
       expect(log).to be_valid
     end
 
+    it "dish_idがなければ無効な状態であること" do
+      log = build(:log, dish_id: nil)
+      expect(log).not_to be_valid
+    end
+  end
 end
