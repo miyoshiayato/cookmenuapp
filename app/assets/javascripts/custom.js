d40676c23de56fee0cb6c2e4341401c962627170
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-12-01 16:16:43 +0900

    custom.jsを読み込み

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,5 +1,6 @@
 var loc = location.href;
 
 var file = loc.substring(loc.lastIndexOf("/")+1,loc.length);
+
 // 現在のURLリンクを持つaタグの親要素(liタグ)にactiveクラスを追加
 $('.header-link').children('a[href$="'+file+'"]').parent().addClass('active');
