ae126e50553caf99f1edb162eb072b7feb823e57
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-12-08 15:20:45 +0900

    (材料の)量の項目追加

--- spec/system/dishes_spec.rb
+++ spec/system/dishes_spec.rb
@@ -31,7 +31,8 @@ RSpec.describe "Dishes", type: :system do
         expect(page).to have_content 'クックメモ'
         expect(page).to have_css 'label[for=dish_ingredients_attributes_0_name]',
                                text: '材料（10種類まで登録可）', count: 1
-        
+        expect(page).to have_css 'label[for=dish_ingredients_attributes_0_quantity]',
+                               text: '量', count: 1
       end
     end
 
