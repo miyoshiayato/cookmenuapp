1895d225bd952a2e63c67b4fe416c2cc3a2d40b9
miyoshiayato <miyoshiayato@yahoo.co.jp>
2020-12-19 22:14:23 +0900

    「みんなの料理」一覧をCSV形式で出力

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -13,7 +13,10 @@ class DishesController < ApplicationController
     # CSV出力時のファイル名指定
     respond_to do |format|
       format.html
-      format.csv 
+      format.csv {
+        send_data render_to_string,
+                  filename: "みんなの料理一覧_#{Time.current.strftime('%Y%m%d_%H%M')}.csv"
+      }
     end
   end
 
